c97594f5baa07d80e21d800f3bddf9249850d493
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-02-14 13:07:12 +0900

    Kiosk_FastFood_Practice_3 메뉴 선택, 기억 하여 주문 내역 표시

---
+++ app/src/main/java/com/example/capstoneapp/component/ItemList.kt
@@ -0,0 +1,95 @@
+package com.example.capstoneapp.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import com.example.capstoneapp.R
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+@Composable
+fun ItemList(selectedMenu: String, onItemClicked: (MenuItem) -> Unit) {
+    Column(
+        modifier = Modifier.padding(16.dp),) {
+        // 선택된 메뉴에 따라 다른 상품 목록을 보여주는 로직
+        val items = when (selectedMenu) {
+            "햄버거" -> listOf(
+                MenuItem("불고기 버거", R.drawable.baseline_adb_24,7000),//R.drawable.ic_bulgogi_burger),
+                MenuItem("새우버거", R.drawable.baseline_adb_24,7000),//R.drawable.ic_shrimp_burger),
+                MenuItem("치즈버거",  R.drawable.baseline_adb_24,7000),//R.drawable.ic_cheese_burger),
+                MenuItem("치킨버거",  R.drawable.baseline_adb_24,7000),//R.drawable.ic_chicken_burger)
+            )
+            // 다른 메뉴에 대한 아이템 리스트 정의
+            else -> listOf()
+        }
+
+        val rows = items.chunked(2) // 리스트를 2개씩 묶어서 새로운 리스트 생성
+        rows.forEach { rowItems ->
+            Row {
+                rowItems.forEach { item ->
+                    ItemCard(item = item) {
+                        onItemClicked(item)
+                    }
+                }
+                if (rowItems.size < 2) { // 항목이 하나만 있는 경우 Spacer 추가로 정렬
+                    Spacer(modifier = Modifier.width(92.dp + 16.dp)) // ItemCard의 너비와 패딩 고려
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemCard(item: MenuItem, onClick: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(18.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Icon(
+            painter = painterResource(id = item.iconResourceId),
+
+            contentDescription = "",
+            modifier = Modifier
+                .width(92.dp) // 아이콘의 너비를 48dp로 설정
+                .height(90.dp)
+        )
+        Text(
+            text = item.name,
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif
+            ),
+            modifier = Modifier.padding(top = 4.dp)
+        )
+        Text(
+            text = item.price.toString() + "원",
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily.SansSerif
+            ),
+            modifier = Modifier.padding(top = 2.dp)
+        )
+    }
+}
+
+data class MenuItem(
+    val name: String,
+    val iconResourceId: Int,
+    val price:Int
+)
---
+++ app/src/main/java/com/example/capstoneapp/component/ItemMenu.kt
@@ -0,0 +1,138 @@
+package com.example.capstoneapp.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.capstoneapp.Frame.kioskButtonFormat
+import com.example.capstoneapp.R
+
+@Composable
+fun itemMenu(selectedMenu: String) {
+    // 주문한 목록
+    val orderItems = remember {
+        mutableStateListOf<MenuItem>()
+    }
+    //테스트용 메뉴 선택 더미 데이터
+    val dummyOrderItems = listOf(
+        MenuItem("불고기 버거", R.drawable.baseline_adb_24, 7000)
+    )
+    //네비게이션 카테고리 선택
+    var selectedMenu by remember { mutableStateOf("햄버거") } // 초기값 설정
+    Column(
+        modifier = Modifier.padding(0.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //네비게이션 bar (추천 메뉴, 햄버거 ...)
+        NavigationBar { menuItem ->
+            selectedMenu = menuItem // 메뉴 항목 클릭 시 선택된 메뉴 업데이트
+        }
+        Divider(
+            color = Color.Gray,
+            thickness = 2.dp,
+            modifier = Modifier.padding(horizontal = 0.dp)
+        )
+        // 메뉴 목록 표시
+        ItemList(selectedMenu = selectedMenu) { selectedItem ->
+            // 아이템 클릭 시 주문 목록에 추가
+            orderItems.add(selectedItem)
+        }
+        Divider(
+            color = Color.Gray,
+            thickness = 2.dp,
+            modifier = Modifier.padding(horizontal = 0.dp)
+        )
+        // 주문 목록 표시
+        orderList(orderItems = dummyOrderItems)
+
+        Divider(
+            color = Color.Gray,
+            thickness = 2.dp,
+            modifier = Modifier.padding(horizontal = 0.dp)
+        )
+
+        Spacer(Modifier.weight(1f)) // This will push the buttons up to be just above the bottom bar
+        // 결제 버튼
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp), // Apply horizontal padding
+            horizontalArrangement = Arrangement.SpaceBetween // Arrange buttons with space in between
+        ) {
+            kioskButtonFormat(
+                modifier = Modifier
+                    .weight(1f)
+                ,
+                onClick = { /* Handle click */ },
+                buttonText = "취소하기",
+                backgroundColor = Color.DarkGray,
+                contentColor = Color.Black
+            )
+            Spacer(modifier = Modifier.width(16.dp)) // Space between buttons
+
+            kioskButtonFormat(
+                modifier = Modifier
+                    .weight(1f)
+                ,
+                onClick = { /* Handle click */ },
+                buttonText = "결제하기",
+                backgroundColor = Color.Red,
+                contentColor = Color.Black
+            )
+            Spacer(modifier = Modifier.height(48.dp))
+        }
+    }
+}
+
+
+
+
+
+@Composable
+fun DefaultMenuPreview() {
+    // 주문 내역에 "불고기 버거" 추가를 시뮬레이션
+    val dummyOrderItems = listOf(
+        MenuItem("불고기 버거", R.drawable.baseline_adb_24, 7000)
+    )
+
+    // AppTheme와 같은 상위 테마 컴포저블이 있으면 여기서 감싸줍니다.
+    var selectedMenu by remember { mutableStateOf("햄버거") } // 초기값 설정
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {// 선택된 메뉴를 기반으로 ItemList 렌더링
+
+        NavigationBar { menuItem ->
+            selectedMenu = menuItem // 메뉴 항목 클릭 시 선택된 메뉴 업데이트
+        }
+        ItemList(selectedMenu = selectedMenu) {
+            // 미리 정의된 주문 내역을 사용하므로 여기서는 아무 작업도 하지 않음
+        }
+        // 구분선
+        Divider(color = Color.Gray, thickness = 2.dp)
+        // 더미 주문 내역을 렌더링
+        orderList(orderItems = dummyOrderItems)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewItemMenu() {
+    itemMenu("햄버거")
+}
