5f592f339a744ff1a00ae22aceebf4fc0d54d0e7
dlthdbs03 <dlsth13@gmail.com>
2024-02-15 22:44:49 +0900

    lsy test

--- app/src/main/java/com/example/capstoneapp/MainActivity.kt
+++ app/src/main/java/com/example/capstoneapp/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.capstoneapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.capstoneapp.Frame.navVar2
 import com.example.capstoneapp.ui.theme.CapstoneAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(topBar = { navVar2() }) {
+                        it
+                    }
                 }
             }
         }
@@ -30,17 +51,98 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "코틀린 엿 먹어;",
         modifier = modifier
     )
+}
+
+@Composable
+fun PictureButton(onClick: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(100.dp))
+        Button(
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(Color(0xFFFFBD42)),
+            modifier = Modifier
+                .width(260.dp) // 너비 설정
+                .height(130.dp) // 높이 설정
+        ) {
+            Text(
+                text = "사진으로 설명보기",
+                color = Color.Black, // 텍스트 색상을 검은색으로 설정
+                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+
+            )
+        }
+    }
+}
+
+@Composable
+fun PracticeButton(onClick: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(Color(0xFFFFDA77)),
+            modifier = Modifier
+                .width(260.dp) // 너비 설정
+                .height(130.dp) // 높이 설정
+                .clip(RectangleShape)
+        ) {
+            Text(
+                text = "직접 연습해보기",
+                color = Color.Black, // 텍스트 색상을 검은색으로 설정
+                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageButton(onClick: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.coffee_img),
+            contentDescription = null, // 이미지에 대한 접근성 설명은 필요하지 않습니다
+            modifier = Modifier
+                .size(194.dp) // 이미지 크기 조정
+                .clickable { onClick() } // 이미지를 클릭하면 지정된 동작을 실행합니다
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CapstoneAppTheme {
-        Greeting("Android")
+        Column {
+            navVar2()
+            //Greeting()
+            PictureButton {
+
+            }
+            PracticeButton {
+
+            }
+            ImageButton {
+                
+            }
+            Spacer(modifier = Modifier.height(100.dp))
+        }
+
+
     }
 }
