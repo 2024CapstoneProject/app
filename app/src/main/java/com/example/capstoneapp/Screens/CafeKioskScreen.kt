2ec3159e8e51c2f9de661e7ebe600467e31f444b
HappyLotus <khyeon2020@naver.com>
2024-02-16 00:12:42 +0900

    파일 폴더 변경 & 포맷 수정
    카페 키오스크 기본 포맷 수정

--- app/src/main/java/com/example/capstoneapp/Screens/CafeKioskScreen.kt
+++ app/src/main/java/com/example/capstoneapp/Screens/CafeKioskScreen.kt
@@ -2,8 +2,8 @@ package com.example.capstoneapp.Screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.capstoneapp.Frame.CafeKioskMainFormat
 import com.example.capstoneapp.Frame.NotificationScreen
+import com.example.capstoneapp.component.CafeKioskMainFormat
 
 
 @Composable
