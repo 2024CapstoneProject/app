b8925a811743d600e7d56f8b551638f0db6d7ec9
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-05-30 20:41:46 +0900

    소윤아 미안해

--- app/src/main/java/com/example/capstoneapp/auth/UserService.kt
+++ app/src/main/java/com/example/capstoneapp/auth/UserService.kt
@@ -1,28 +1,39 @@
 package com.example.capstoneapp.auth
 
-import retrofit2.Call
+
+import com.google.gson.GsonBuilder
+import retrofit2.Response
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.Header
+import retrofit2.http.Query
+
 
 interface UserService {
 
-    @POST("api/sendToken")
-    fun sendToken(@Body token: String): Call<Void>
+    @POST("/api/auth/login")
+    suspend fun loginUser(
+        @Query("uid") uid: String,
+        @Query("email") email: String
+    ): Response<Void>
 
-    @POST("api/validateToken")
-    fun validateToken(@Header("Authorization") token: String): Call<Boolean>
 
-    companion object {
-        fun create(): UserService {
-            val retrofit = Retrofit.Builder()
-                .baseUrl("http://118.67.135.211:9000/")
-                .addConverterFactory(GsonConverterFactory.create())
+    object RetrofitInstance {
+        private val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        private val retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl("http://118.67.135.211:9000") // 백엔드 서버의 기본 URL을 설정하세요.
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
 
-            return retrofit.create(UserService::class.java)
+        }
+
+        val api: UserService by lazy {
+            retrofit.create(UserService::class.java)
         }
     }
 }
