9927db77efea13fc2887092272d5f831341696a7
HappyLotus <khyeon2020@naver.com>
2024-08-13 17:56:41 +0900

    카카오톡, 패스트푸드 선택 화면 수정

--- app/src/main/java/com/example/capstoneapp/fastfood/ui/screens/FastFoodHomeScreen.kt
+++ app/src/main/java/com/example/capstoneapp/fastfood/ui/screens/FastFoodHomeScreen.kt
@@ -1,41 +1,178 @@
 package com.example.capstoneapp.fastfood.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.capstoneapp.cafe.ui.theme.CapstoneAppTheme
 
 @Composable
 fun FastFoodHomeScreen(navController: NavController) {
-    com.example.capstoneapp.cafe.ui.theme.CapstoneAppTheme {
+    CapstoneAppTheme {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // 가이드모드
-            PictureButton(
+            Box(
                 modifier = Modifier
-                    .weight(1f)
+                    .height(80.dp)
                     .fillMaxWidth(),
-                onClick = { navController.navigate("HamburgerGuideScreen") }
-            )
-            // 연습모드
-            PracticeButton(
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    IconButton(
+                        modifier = Modifier.size(56.dp),
+                        onClick = { navController.navigate("Guide0") },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowLeft,
+                            contentDescription = "",
+                            tint = Color.Black,
+                            modifier = Modifier.size(56.dp)
+                        )
+
+                    }
+
+                    Text(
+                        text = "패스트푸드",
+                        fontSize = 32.sp,
+                        color = Color.Black,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
+
+            Column(
                 modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth(),
-                onClick = { navController.navigate("HamburgerPracticeHomeScreen") }
-            )
+                    .fillMaxSize(1f)
+                    .padding(bottom = 24.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                OptionButton(
+                    navController = navController,
+                    paddingValue = 16.dp,
+                    imageVector = Icons.Default.Warning,
+                    title = "사진 설명서",
+                    description = "사진으로 설명서를 보여줘요.",
+                    route = "HamburgerGuideScreen"
+                )
+                OptionButton(
+                    navController = navController,
+                    paddingValue = 0.dp,
+                    imageVector = Icons.Default.Warning,
+                    title = "연습해보기",
+                    description = "직접 문제를 풀며 연습해요.",
+                    route = "HamburgerPracticeHomeScreen"
+                )
+            }
+
         }
     }
+}
+
+@Composable
+fun OptionButton(
+    navController: NavController,
+    paddingValue: Dp,
+    imageVector: ImageVector,
+    title: String,
+    description: String,
+    route: String
+) {
+    Button(
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 16.dp,
+        ),
+        contentPadding = PaddingValues(),
+        onClick = {
+            navController.navigate(route)
+        },
+        modifier = Modifier
+            .width(360.dp)
+            .height(320.dp)
+            .padding(bottom = paddingValue),
+        colors = ButtonDefaults.buttonColors(Color(0xFFFFDA77)),
+        shape = RoundedCornerShape(28.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .wrapContentSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = imageVector,
+                    contentDescription = "",
+                    tint = Color(0xFF5C460C),
+                    modifier = Modifier.size(72.dp)
+                )
+                Text(
+                    text = title,
+                    color = Color.Black,
+                    style = MaterialTheme.typography.displayLarge,
+                    fontSize = 32.sp,
+                    modifier = Modifier.padding(top = 20.dp, bottom = 20.dp)
+                )
+                Text(
+                    text = description,
+                    color = Color(0xFFADADAD),
+                    style = MaterialTheme.typography.displayLarge,
+                    fontSize = 24.sp,
+                )
+            }
+
+        }
+
+    }
+
 }
 
 @Composable
@@ -58,7 +195,7 @@ fun PracticeButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun fastFoodHomeScreenPreview() {
     val navController = rememberNavController()
